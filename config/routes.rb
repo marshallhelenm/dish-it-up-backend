cc7f253d8284dee89b4bd8749957ff4e85a42226
marshallhelenm <marshallhelenm@gmail.com>
2019-09-27 14:25:45 -0700

    working on login

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'getrecipes', to: 'getrecipes#get_recipes'
+  post 'login', to: 'users#login'
+
 end
